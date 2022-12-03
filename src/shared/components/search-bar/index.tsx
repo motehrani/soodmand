035fb0d8acd829d5a9819aed67298df442ffdd87
htehrani98 <h.tehrani1377@gmail.com>
2022-12-04 02:20:49 +0330

    feat(home): Create navbar component

---
+++ src/shared/components/search-bar/index.tsx
@@ -0,0 +1,11 @@
+const SearchBar = () => {
+  return (
+    <div>
+      <div className="grouping"></div>
+      <div className="search-box"></div>
+      <div className="offers"></div>
+    </div>
+  )
+}
+
+export default SearchBar
