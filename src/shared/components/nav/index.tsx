c32256e0dfa4249b37ffd911f1a9689fd3bb7758
htehrani98 <h.tehrani1377@gmail.com>
2022-12-24 22:39:32 +0330

    Draft(global: Install reactstrap

--- src/shared/components/nav/index.tsx
+++ src/shared/components/nav/index.tsx
@@ -1,61 +1,60 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import { Container, Row, Col } from 'reactstrap'
+import { Button } from 'reactstrap'
 
 import { QuickAccess } from './quick-access'
 
 export const Nav = () => {
   return (
     <nav className="nav-bar">
-      <Container>
-        <Row>
-          <Col className="icon">
-            <Image src="/svg/book.svg" alt="LOGO" width={25} height={25} />
-            <Image src="/svg/heart.svg" alt="LOGO" width={25} height={25} />
-            <Image src="/svg/person.svg" alt="LOGO" width={25} height={25} />
-          </Col>
+      <div>
+        <div className="icon">
+          <Image src="/svg/book.svg" alt="LOGO" width={25} height={25} />
+          <Image src="/svg/heart.svg" alt="LOGO" width={25} height={25} />
+          <Image src="/svg/person.svg" alt="LOGO" width={25} height={25} />
+        </div>
 
-          <Col className="navigators">
-            <div>
-              <QuickAccess
-                title="پشتیبانی"
-                description="09103381006"
-                image="/svg/call.svg"
-              />
+        <div className="navigators">
+          <Button color="primary">some</Button>
+          <div>
+            <QuickAccess
+              title="پشتیبانی"
+              description="09103381006"
+              image="/svg/call.svg"
+            />
 
-              <QuickAccess
-                title="فرصت محدود"
-                description="پیشنهاد شگفت انگیز"
-                image="/svg/discount.svg"
-              />
+            <QuickAccess
+              title="فرصت محدود"
+              description="پیشنهاد شگفت انگیز"
+              image="/svg/discount.svg"
+            />
 
-              <ul className="catagories">
-                <li>
-                  <Link href="/">خانه</Link>
-                </li>
-                <li>
-                  <Link href="/">فروشگاه</Link>
-                </li>
-                <li>
-                  <Link href="/">صفحات</Link>
-                </li>
-                <li>
-                  <Link href="/">بلاگ</Link>
-                </li>
-                <li>
-                  <Link href="/">ارتباط با ما</Link>
-                </li>
-              </ul>
-            </div>
-          </Col>
+            <ul className="catagories">
+              <li>
+                <Link href="/">خانه</Link>
+              </li>
+              <li>
+                <Link href="/">فروشگاه</Link>
+              </li>
+              <li>
+                <Link href="/">صفحات</Link>
+              </li>
+              <li>
+                <Link href="/">بلاگ</Link>
+              </li>
+              <li>
+                <Link href="/">ارتباط با ما</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
 
-          <Col className="logo">
-            <Link href="/" id="logo">
-              <Image src="/img/sood.png" alt="LOGO" width={100} height={50} />
-            </Link>
-          </Col>
-        </Row>
-      </Container>
+        <div className="logo">
+          <Link href="/" id="logo">
+            <Image src="/img/sood.png" alt="LOGO" width={100} height={50} />
+          </Link>
+        </div>
+      </div>
     </nav>
   )
 }
