15ed59ab14ee3a41894542af3e9dd3dbbd11d0dc
motehrani <h.tehrani1377@gmail.com>
2022-12-26 21:02:32 +0330

    feat(home): Move slider to components

--- src/shared/components/opening/components.tsx
+++ src/shared/components/opening/components.tsx
@@ -1,6 +1,8 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+
+import classNames from 'classnames'
 
 export interface FacilityProps {
   title: string
@@ -20,9 +22,25 @@ export const Facility: FC<FacilityProps> = ({ title, description, image }) => {
   )
 }
 
-export const Slide: FC<FacilityProps> = ({ title, description, image }) => {
+export interface SlideProps {
+  title: string
+  description: string
+  image: string
+  isActive: boolean
+}
+
+export const Slide: FC<SlideProps> = ({
+  title,
+  description,
+  image,
+  isActive,
+}) => {
+  const containerClassNames = classNames('slide', {
+    active: isActive,
+  })
+
   return (
-    <div className="slide">
+    <div className={containerClassNames}>
       <Link href="/" id="opening">
         <Image alt="opening" src={image} width={1366} height={512} />
         <div className="info">
@@ -30,6 +48,54 @@ export const Slide: FC<FacilityProps> = ({ title, description, image }) => {
           <p>{description}</p>
         </div>
       </Link>
+    </div>
+  )
+}
+
+export interface SliderProps {
+  slides: Omit<SlideProps, 'isActive'>[]
+}
+
+export const Slider: FC<SliderProps> = ({ slides }) => {
+  const [activeSlide, setActiveSlide] = useState(0)
+
+  const getNavigationClassNames = (index: number) => {
+    return classNames('slide-icon', {
+      active: index === activeSlide,
+    })
+  }
+
+  const goToNextSlide = () => {
+    setActiveSlide(currentActiveSlide => {
+      return currentActiveSlide + 1
+    })
+  }
+
+  const goToPrevSlide = () => {
+    setActiveSlide(currentActiveSlide => {
+      return currentActiveSlide - 1
+    })
+  }
+
+  return (
+    <div className="slider">
+      {slides.map((slide, index) => (
+        <Slide key={index} isActive={index === activeSlide} {...slide} />
+      ))}
+
+      <div className="navigation">
+        <i onClick={goToPrevSlide} className="fas fa-chevron-left prev-btn"></i>
+        <i
+          onClick={goToNextSlide}
+          className="fas fa-chevron-right next-btn"
+        ></i>
+      </div>
+
+      <div className="navigation-visibility">
+        {slides.map((slide, index) => (
+          <div className={getNavigationClassNames(index)} key={index}></div>
+        ))}
+      </div>
     </div>
   )
 }
--- src/shared/components/opening/index.tsx
+++ src/shared/components/opening/index.tsx
@@ -1,50 +1,43 @@
-import Link from 'next/link'
-import Image from 'next/image'
-
-import { Facility } from './components'
-import { Slide } from './components'
+import { Facility, Slider } from './components'
 
 export const Opening = () => {
   return (
     <div className="opening">
-      <div className="slider">
-        <Slide
-          image="/img/slider/a.jpg"
-          title="dastan chie 1"
-          description="Lorem ipsum dolor, sit amet consectetur adipisicing elit."
-        />
-        <Slide
-          image="/img/slider/b.jpg"
-          title="dastan chie 2"
-          description="Lorem ipsum dolor, sit amet consectetur adipisicing elit."
-        />
-        <Slide
-          image="/img/slider/c.jpg"
-          title="dastan chie 3"
-          description="Lorem ipsum dolor, sit amet consectetur adipisicing elit."
-        />
-        <Slide
-          image="/img/slider/d.jpg"
-          title="dastan chie 4"
-          description="Lorem ipsum dolor, sit amet consectetur adipisicing elit."
-        />
-        <Slide
-          image="/img/slider/e.jpg"
-          title="dastan chie 5"
-          description="Lorem ipsum dolor, sit amet consectetur adipisicing elit."
-        />
-        <div className="navigation">
-          <i className="fas fa-chevron-left prev-btn"></i>
-          <i className="fas fa-chevron-right next-btn"></i>
-        </div>
-        <div className="navigation-visibility">
-          <div className="slide-icon"></div>
-          <div className="slide-icon"></div>
-          <div className="slide-icon"></div>
-          <div className="slide-icon"></div>
-          <div className="slide-icon"></div>
-        </div>
-      </div>
+      <Slider
+        slides={[
+          {
+            image: '/img/slider/a.jpg',
+            title: 'dastan chie 1',
+            description:
+              'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+          },
+          {
+            image: '/img/slider/b.jpg',
+            title: 'dastan chie 2',
+            description:
+              'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+          },
+          {
+            image: '/img/slider/c.jpg',
+            title: 'dastan chie 3',
+            description:
+              'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+          },
+          {
+            image: '/img/slider/d.jpg',
+            title: 'dastan chie 4',
+            description:
+              'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+          },
+          {
+            image: '/img/slider/e.jpg',
+            title: 'dastan chie 5',
+            description:
+              'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+          },
+        ]}
+      />
+
       <div className="facilities">
         <Facility
           title="ارسال رایگان"
