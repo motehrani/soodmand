067205d36af5391b0e2e1fe49d4d8a43322d3f6a
htehrani98 <h.tehrani1377@gmail.com>
2023-01-19 16:21:16 +0330

    draft(home):  fix responsive footer component

--- src/shared/components/footer/index.tsx
+++ src/shared/components/footer/index.tsx
@@ -5,19 +5,9 @@ export const Footer = () => {
   return (
     <footer>
       <Container>
-        <Row>
+        <Row className="footer-row">
           <div className="footer">
-            <Col xxl="1" xl="1" lg="1" md="1" sm="1" xs="1">
-              <div className="quick-access">
-                <li>منوی سریع</li>
-                <h2>فروشگاه</h2>
-                <h2>پرداخت</h2>
-                <h2>سبد خرید</h2>
-                <h2>درباره ما</h2>
-                <h2>تماس با ما</h2>
-              </div>
-            </Col>
-            <Col xxl="2" xl="2" lg="2" md="2" sm="2" xs="2">
+            <Col xxl="3" xl="3" lg="3" md="3" sm="3" xs="3">
               <div className="contact">
                 <li>اطلاعات تماس</li>
                 <div className="address">
@@ -36,12 +26,6 @@ export const Footer = () => {
             </Col>
             <Col xxl="3" xl="3" lg="3" md="3" sm="3" xs="3">
               <div className="join">
-                <li>عضویت در خبرنامه</li>
-                <h2>برای اطلاع از تخفیفات و اخبار روز، عضو خبرنامه ما شوید</h2>
-                <div className="input">
-                  <span className="material-icons">contact_mail</span>
-                  <input type="text" placeholder="ایمیل را وارد نمایید" />
-                </div>
                 <li>شبکه های اجتماعی</li>
                 <div className="sm">
                   <Image
@@ -71,7 +55,7 @@ export const Footer = () => {
                 </div>
               </div>
             </Col>
-            <Col xxl="4" xl="4" lg="4" md="4" sm="4" xs="4">
+            <Col xxl="3" xl="3" lg="3" md="3" sm="3" xs="3">
               <div className="license">
                 <li>مجوز ها</li>
                 <Image
