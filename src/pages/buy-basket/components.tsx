54fe5aecae818d713b13b9aa942df79dd94807b1
htehrani98 <h.tehrani1377@gmail.com>
2023-04-09 02:12:49 +0430

    add buy basket page

--- src/pages/buy-basket/components.tsx
+++ src/pages/buy-basket/components.tsx
@@ -2,11 +2,7 @@ export const NumberOfProduct = () => {
   return (
     <div className="number-of-product">
       <div className="line-id">
-        <div className="vertical-line"></div>
-        <div className="id">
-          <h2>1</h2>
-        </div>
-        <div className="vertical-line"></div>
+        <h2>1</h2>
       </div>
       <div className="picture">
         <img src="/img/product.jpg" alt="" />
