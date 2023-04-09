4ed9917928933f40bccf82d42a29f15eca101ea7
htehrani98 <h.tehrani1377@gmail.com>
2023-04-09 12:36:12 +0430

    add buy basket page

--- src/pages/buy-basket/components.tsx
+++ src/pages/buy-basket/components.tsx
@@ -7,7 +7,29 @@ export const NumberOfProduct = () => {
       <div className="picture">
         <img src="/img/product.jpg" alt="" />
       </div>
-      <div className="detail"></div>
+      <div className="detail">
+        <div className="name-and-id">
+          <h2 className="name">شیرآلات نقره ای خم دار</h2>
+          <h2 className="id">(کد محصول B7DFH239)</h2>
+        </div>
+        <div className="color-size-weight">
+          <div className="color">
+            <h2 className="title">رنگ:</h2>
+            <h2 className="description"></h2>
+          </div>
+          <div className="size">
+            <h2 className="title">سایز:</h2>
+            <h2 className="description"></h2>
+          </div>
+          <div className="weight">
+            <h2 className="title">وزن:</h2>
+            <h2 className="description">250 گرم</h2>
+          </div>
+        </div>
+        <div className="price">
+          <h2>420.000 تومان</h2>
+        </div>
+      </div>
       <div className="delete-number"></div>
     </div>
   )
